c1304189869a5f3d79c4d7e903082b637fcb5be9
dc <diabl456@naver.com>
2024-03-21 17:43:05 +0900

    pratice board

--- JDBCClass/jdbcPro/src/days04/Ex01.java
+++ JDBCClass/jdbcPro/src/days04/Ex01.java
@@ -15,7 +15,7 @@ import com.util.DBConn;
 import domain.DeptEmpVO;
 import domain.EmpVO2;
 
-public class Tr {
+public class Ex01 {
 
 	public static void main(String[] args) {
 		/*
---
+++ JDBCClass/jdbcPro/src/test/board/BoardDAOImplTest.java
@@ -0,0 +1,13 @@
+package test.board;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardDAOImplTest {
+
+	@Test
+	void test() {
+	}
+
+}
